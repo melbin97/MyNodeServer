eb48e2ff81211beb39b1617ea91a2061cfefcc48
Melbin Mathew <mmathew@trl.co.uk>
2021-09-12 16:51:17 +0530

    framework setup

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 import express, { json } from 'express';
 import morgan from 'morgan';
 import { connect } from 'mongoose';
-require('dotenv').config()
+import restrictOrigin from './middlewares/restrictOrigin';
+require('dotenv').config();
 
-const app = new express()
+const app = new express();
 const PORT = process.env.PORT || 5000;
 
 connect(process.env.MONGO_URL)
-        .then(()=>{
-            console.log('succesfully connected to mongoose....')
-        })
-        .catch((ex)=>{
-            console.log('Error in connection with exception: ', ex)
-        })
-app.use(json())
-app.use(morgan("dev"))
+	.then(()=>{
+		console.log('succesfully connected to mongoose....');
+	})
+	.catch((ex)=>{
+		console.log('Error in connection with exception: ', ex);
+	})
+app.use(restrictOrigin);
+app.use(morgan('dev'));
 
-app.get("/ping", (req,res)=>{
-    return res.send({
-        status: 'healthy'
-    });
+app.get('/ping', (req,res)=>{
+	return res.send({
+		status: 'healthy'
+	});
 });
 
 app.listen(PORT, () => {
-    console.log("server listening on PORT: ", PORT)
-})
+	console.log('server listening on PORT: ', PORT);
+});
 
